c8da38d5da5d0361b3d0bf66bb9b89b0277cf054
Shashi <shashiross9@gmail.com>
2026-06-06 16:04:29 +0530

    feat: enhance main.ts with ConsoleLogger configuration and add DOM types to tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { ConsoleLogger, Logger, ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './logging.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const logger = new Logger();
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      json: true,
+      colors: true,
+    }),
+  });
+  app.useGlobalPipes(new ValidationPipe());
 
   const port = process.env.PORT;
 
   app.useGlobalInterceptors(new LoggingInterceptor());
 
   await app.listen(port ?? 3000);
-
-  logger.log(`Server running at ${port ?? 3000}`);
 }
 bootstrap();
